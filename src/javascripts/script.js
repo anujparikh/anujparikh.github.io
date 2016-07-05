d5b96fed11a67ff0a067cd703a8ebdcda5979aab
anujparikh <anujparikh1@gmail.com>
2016-07-05 14:42:36 -0500

    change for radial progress bar

--- src/javascripts/script.js
+++ src/javascripts/script.js
@@ -15,8 +15,20 @@
         }
     }, 250);
 
+    $(window).on('scroll.custom', function () {
+        var height = $(this).scrollTop();
+
+        if (height > 1000) {
+            drawRadialProgressBar();
+            $(window).off('scroll.custom');
+        }
+    })
+
     function hasScrolled() {
         var st = $(this).scrollTop();
+        console.log('st: ', st);
+        console.log('lastScrollTop: ', lastScrollTop);
+
         if (Math.abs(lastScrollTop - st) <= delta) {
             return;
         }
@@ -43,61 +55,62 @@
         }, 1000);
     });
 
-    var canvas = document.getElementsByTagName('canvas');
+    function drawRadialProgressBar() {
+        var canvas = document.getElementsByTagName('canvas');
 
-    for (var i = 0; i < canvas.length; i++) {
-        progressBar(canvas[i].id);
-    }
-
-    function progressBar(canvasId) {
-        var degreesCall;
-        var canvas = document.getElementById(canvasId);
-        var ctx = canvas.getContext('2d');
-        var cWidth = canvas.width;
-        var cHeight = canvas.height;
-        var progressColor = canvas.getAttribute('data-progress-bar-color');
-        var circleColor = 'lightgray';
-        var rawPerc = canvas.getAttribute('data-perc');
-        var definition = canvas.getAttribute('data-text');
-        var perc = parseInt(rawPerc);
-        var degrees = 0;
-        var endDegrees = (360*perc)/100;
-
-        var lineWidth = 20;
-
-        function getDegrees() {
-            if(degrees < endDegrees) {
-                degrees++;
-            }
-            else {
-                clearInterval(degreesCall);
-            }
-            drawProgressBar();
+        for (var i = 0; i < canvas.length; i++) {
+            progressBar(canvas[i].id);
         }
 
-        function drawProgressBar() {
-            ctx.clearRect(0,0,cWidth,cHeight);
-            ctx.beginPath();
-            ctx.strokeStyle = circleColor;
-            ctx.lineWidth = lineWidth -1;
-            ctx.arc(cHeight/2, cWidth/2, cWidth/3, 0, Math.PI*2, false);
-            ctx.stroke();
-            var radians = 0;
-            radians = degrees * Math.PI/180;
-            ctx.beginPath();
-            ctx.strokeStyle = progressColor;
-            ctx.lineWidth = lineWidth;
-            ctx.arc(cHeight/2, cWidth/2, cWidth/3, 0 - 90*Math.PI/180, radians - 90*Math.PI/180, false);
-            ctx.stroke();
-            ctx.fillStyle = '#A2A29C';
-            ctx.font = "bold 15px 'Open Sans', sans-serif";
-            var outputTextPerc = Math.floor(degrees/360*100)+'%';
-            var outputTextPercWidth = ctx.measureText(outputTextPerc).width;
-            var outputTextDefinitionWidth = ctx.measureText(definition).width;
-            // ctx.fillText(outputTextPerc, cWidth/2 - outputTextPercWidth/2, cHeight/2 - 10);
-            ctx.fillText(definition, cWidth/2 - outputTextDefinitionWidth/2, cHeight/2 + 5);
-        }
+        function progressBar(canvasId) {
+            var degreesCall;
+            var canvas = document.getElementById(canvasId);
+            var ctx = canvas.getContext('2d');
+            var cWidth = canvas.width;
+            var cHeight = canvas.height;
+            var progressColor = canvas.getAttribute('data-progress-bar-color');
+            var circleColor = 'lightgray';
+            var rawPerc = canvas.getAttribute('data-perc');
+            var definition = canvas.getAttribute('data-text');
+            var perc = parseInt(rawPerc);
+            var degrees = 0;
+            var endDegrees = (360 * perc) / 100;
 
-        degreesCall = setInterval(getDegrees, 10/(degrees - endDegrees));
+            var lineWidth = 20;
+
+            function getDegrees() {
+                if (degrees < endDegrees) {
+                    degrees++;
+                }
+                else {
+                    clearInterval(degreesCall);
+                }
+                drawProgressBar();
+            }
+
+            function drawProgressBar() {
+                ctx.clearRect(0, 0, cWidth, cHeight);
+                ctx.beginPath();
+                ctx.strokeStyle = circleColor;
+                ctx.lineWidth = lineWidth - 1;
+                ctx.arc(cHeight / 2, cWidth / 2, cWidth / 3, 0, Math.PI * 2, false);
+                ctx.stroke();
+                var radians = 0;
+                radians = degrees * Math.PI / 180;
+                ctx.beginPath();
+                ctx.strokeStyle = progressColor;
+                ctx.lineWidth = lineWidth;
+                ctx.arc(cHeight / 2, cWidth / 2, cWidth / 3, 0 - 90 * Math.PI / 180, radians - 90 * Math.PI / 180, false);
+                ctx.stroke();
+                ctx.fillStyle = '#A2A29C';
+                ctx.font = "bold 15px 'Open Sans', sans-serif";
+                var outputTextPerc = Math.floor(degrees / 360 * 100) + '%';
+                var outputTextPercWidth = ctx.measureText(outputTextPerc).width;
+                var outputTextDefinitionWidth = ctx.measureText(definition).width;
+                ctx.fillText(definition, cWidth / 2 - outputTextDefinitionWidth / 2, cHeight / 2 + 5);
+            }
+
+            degreesCall = setInterval(getDegrees, 10 / (degrees - endDegrees));
+        }
     }
 }(jQuery));
